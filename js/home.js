0d0510cb4a1aa19f1755d59875039f5b7b434ed9
amandavarella <lab245>
2014-04-12 21:18:56 -0300

    Primeira versão
    Não responsiva

---
+++ js/home.js
@@ -0,0 +1,88 @@
+function pad(num, size) {
+    var s = num+"";
+    while (s.length < size) s = "0" + s;
+    return s;
+}
+
+function calculaHoraDormir(){
+	var inputTime=document.querySelector('#time').value;
+	var timeAnswers="";
+	var horasDormir = new Array();
+
+
+	for(var i=0; i<8; i++)
+	{
+		var res = inputTime.split(":");
+		var hora = res[0];
+		var minuto = res[1];	
+		var hourCycle1 = parseInt(hora) - 2;
+		var minutesCycle1 = parseInt(minuto) + 30;
+
+		
+		if(minutesCycle1 >=60){
+			minutesCycle1 = minutesCycle1%60;
+		    hourCycle1 = hourCycle1 +1;
+		}
+		
+		hourCycle1 = hourCycle1%24;
+		if(hourCycle1 <0)
+			hourCycle1=hourCycle1 + 24;
+
+		
+		
+		var nextCycle = pad(hourCycle1,2) + ":" + pad(minutesCycle1,2);
+			
+		
+		if(i<6 && i>1){
+
+			horasDormir.push(nextCycle);
+		}
+
+		inputTime = nextCycle;
+
+	}
+
+	$("#label").text(horasDormir.reverse());
+
+	return false;
+
+}
+
+function calculaHoraAcordar(){
+	var inputTime=document.querySelector('#time').value;
+	var timeAnswers="";
+
+
+	for(var i=0; i<8; i++)
+	{
+		var res = inputTime.split(":");
+		var hora = res[0];
+		var minuto = res[1];	
+		var hourCycle1 = parseInt(hora) + 1;
+		var minutesCycle1 = parseInt(minuto) + 30;
+
+		
+		if(minutesCycle1 >=60){
+			minutesCycle1 = minutesCycle1%60;
+		    hourCycle1 = hourCycle1 +1;
+		}
+		
+		hourCycle1 = hourCycle1%24;
+
+		var nextCycle = pad(hourCycle1,2) + ":" + pad(minutesCycle1,2);
+		
+		if(i>3)		
+			timeAnswers = timeAnswers + nextCycle + ",";
+		inputTime = nextCycle;
+
+	}
+	document.querySelector('#label').innerHTML = timeAnswers.replace(/,$/, "");
+
+
+	return false;
+}
+
+document.querySelector('#btnAcordar').onclick=calculaHoraAcordar;
+document.querySelector('#btnDormir').onclick=calculaHoraDormir;
+
+
